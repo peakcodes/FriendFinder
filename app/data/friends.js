64c359875c349e4e5cc6fc3eb3d4d7e25f1d8f1f
Tim Hill <peakcodes@gmail.com>
2018-03-12 23:45:24 -0400

    more updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,8 @@
+
+
 var friends = [
 {
-    "name":"Ahmed",
+    "name":"Ahmed Cholo",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         1,
@@ -16,7 +18,7 @@ var friends = [
       ]
   },
 {
-    "name":"Joe",
+    "name":"Joe Rick",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
@@ -30,5 +32,55 @@ var friends = [
         5,
         5
       ]
+},
+{
+    "name":"Mr. Nobody",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        3,
+        2,
+        2,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4
+      ]
+},
+{
+    "name":"Adam Sandler",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        4,
+        3,
+        2,
+        1,
+        5,
+        4,
+        3,
+        2,
+        1
+      ]
+},
+{
+    "name":"Sants Claus",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        2,
+        3,
+        4,
+        5,
+        1,
+        2,
+        3,
+        4,
+        5
+      ]
 }]
+
+module.exports = friends; 
 
