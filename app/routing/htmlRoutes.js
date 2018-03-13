c8f2432ab5522aed62b526c0368c4d0179e2543a
Tim Hill <peakcodes@gmail.com>
2018-03-12 21:43:12 -0400

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,11 +3,11 @@ var path = require('path');
 module.exports = function(app) {
 //    get request hanlder for root / catch all
 app.get('/', function(req, res) {
-    res.sendFile(path.join(_dirname, '../public/home.html'))
+    res.sendFile(path.join(_dirname, './public/home.html'))
 });
 
 app.get('/survey', function(req, res){
-    res.sendFile(path.join(_dirname, '../public/survey.html'))
+    res.sendFile(path.join(_dirname, './public/survey.html'))
 })
 }
 // get request to route to survey
